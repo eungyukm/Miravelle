4674a40b994a5a198c22591d8009800e3f132c93
배민경 <bmk0703@naver.com>
2025-03-02 16:59:50 +0900

    feat : add logout url

--- core/users/urls.py
+++ core/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import login
+from .views import login, logout
 
 urlpatterns = [
-    path('login/', login, name='login'),
+    path('login/', login, name='login'), # 로그인
+    path('logout/', logout, name='logout'), # 로그아웃
 ]
