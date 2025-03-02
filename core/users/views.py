3ef9b3802e251ddddc9ca04171254786d4e6ae16
배민경 <bmk0703@naver.com>
2025-03-02 16:19:09 +0900

    fix : login view code

--- core/users/views.py
+++ core/users/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 
-def login_view(request):
-    return render(request, 'login.html')
+# 로그인
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            # Log the user in
+            auth_login(request, form.get_user())   # 로그인 하기
+            next_url = request.GET.get("next") or "main"
+            return redirect(next_url)
+    
+    else:    
+        form = AuthenticationForm()
+    context = {'form': form}
+    return render(request, 'login.html', context)
