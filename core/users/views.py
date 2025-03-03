55739c319c4e860049aa478146ff8af989132d69
배민경 <bmk0703@naver.com>
2025-03-03 13:59:35 +0900

    feat : add main view code

--- core/users/views.py
+++ core/users/views.py
@@ -4,6 +4,10 @@ from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.decorators import login_required
 
+# 메인 화면
+def main(request):
+    return render(request, "main.html")
+
 # 로그인
 def login(request):
     if request.method == "POST":
@@ -11,17 +15,17 @@ def login(request):
         if form.is_valid():
             # Log the user in
             auth_login(request, form.get_user())   # 로그인 하기
-            next_url = request.GET.get("next") or "main"
+            next_url = request.GET.get("users:main")
             return redirect(next_url)
     
     else:    
         form = AuthenticationForm()
-    context = {'form': form}
-    return render(request, 'login.html', context)
+    context = {"form": form}
+    return render(request, "login.html", context)
 
 
 # 로그아웃
 @login_required
 def logout(request):
     auth_logout(request)    # 로그아웃 하기
-    return redirect('main')
+    return redirect("main")
