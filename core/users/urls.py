49ece5fd89a60c47563c50c1a62ce59eafb747d7
배민경 <bmk0703@naver.com>
2025-03-03 13:59:17 +0900

    feat : add main page url

--- core/users/urls.py
+++ core/users/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path, include
-from .views import login, logout
+from django.urls import path
+from .views import main, login, logout
 
+app_name = "users"
 urlpatterns = [
-    path('login/', login, name='login'), # 로그인
-    path('logout/', logout, name='logout'), # 로그아웃
+    path("main/", main, name="main"), # 메인 화면
+    path("login/", login, name="login"), # 로그인
+    path("logout/", logout, name="logout"), # 로그아웃
 ]
