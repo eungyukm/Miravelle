a1b139784a38838ad13f64c0cb3bcc2b1f1e4581
배민경 <bmk0703@naver.com>
2025-03-03 14:53:41 +0900

    feat : add surname, email field in User model

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -3,4 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    surname = models.CharField(max_length=50) # 성씨
+    email = models.EmailField(unique=True) # 중복 불가
     message = models.TextField()
