a516f34f6e069fb5ee8a213e1038d701e0bf5f75
배민경 <bmk0703@naver.com>
2025-03-03 14:30:01 +0900

    fix : add CustomAuthenticationsForm

--- core/users/views.py
+++ core/users/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.decorators import login_required
+from .forms import CustomAuthenticationForm
 
 # 메인 화면
 def main(request):
@@ -11,15 +11,15 @@ def main(request):
 # 로그인
 def login(request):
     if request.method == "POST":
-        form = AuthenticationForm(data=request.POST)
+        form = CustomAuthenticationForm(data=request.POST) # CustomAuthenticationForm 사용
         if form.is_valid():
             # Log the user in
             auth_login(request, form.get_user())   # 로그인 하기
-            next_url = request.GET.get("users:main")
-            return redirect(next_url)
+            next_url = request.GET.get("next")  # next 인자 제대로 가져오기
+            return redirect(next_url or "main")  # next가 없으면 main으로 리다이렉트
     
     else:    
-        form = AuthenticationForm()
+        form = CustomAuthenticationForm()  # CustomAuthenticationForm 사용
     context = {"form": form}
     return render(request, "login.html", context)
 
