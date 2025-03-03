dec7491c88140a984d5dd919cc9885143a67bd91
배민경 <bmk0703@naver.com>
2025-03-03 15:34:47 +0900

    fix : add password1, 2 field and username's label=ID, surname's label=Name

--- core/accounts/forms.py
+++ core/accounts/forms.py
@@ -2,13 +2,14 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 from django import forms
 
-# 회원 가입 form ()
 class CustomUserCreationForm(UserCreationForm):
-    username = forms.CharField(max_length=50, label="Name")
-    surname = forms.CharField(max_length=50, label="Surname")
+    username = forms.CharField(max_length=50, label="ID")
+    surname = forms.CharField(max_length=50, label="Name")
+    password1 = forms.CharField(widget=forms.PasswordInput, label="Password")
+    password2 = forms.CharField(widget=forms.PasswordInput, label="Password Confirmation")
     email = forms.EmailField(label="Email")
-    message = forms.CharField(widget=forms.Textarea, label="Message", required=False) # 필수 항목 아님
+    message = forms.CharField(widget=forms.Textarea, label="Message", required=False)
 
-    class Meta:
+    class Meta(UserCreationForm.Meta):
         model = get_user_model()
-        fields = ("username", "surname", "email", "message") + UserCreationForm.Meta.fields
+        fields = ("username", "surname", "password1", "password2", "email", "message")
