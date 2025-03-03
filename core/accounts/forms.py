2da6c26f20c58163bbddf7a396999ada0d6358a7
배민경 <bmk0703@naver.com>
2025-03-03 14:57:06 +0900

    fix : register form

--- core/accounts/forms.py
+++ core/accounts/forms.py
@@ -1,9 +1,14 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
-from django.urls import reverse
+from django import forms
 
+# 회원 가입 form ()
 class CustomUserCreationForm(UserCreationForm):
-    
+    username = forms.CharField(max_length=50, label="Name")
+    surname = forms.CharField(max_length=50, label="Surname")
+    email = forms.EmailField(label="Email")
+    message = forms.CharField(widget=forms.Textarea, label="Message", required=False) # 필수 항목 아님
+
     class Meta:
         model = get_user_model()
-        fields = UserCreationForm.Meta.fields + ()
+        fields = ("username", "surname", "email", "message") + UserCreationForm.Meta.fields
