790fce19ab4691276e2c69cd7f7395a96c1a5341
배민경 <bmk0703@naver.com>
2025-03-03 16:55:05 +0900

    fix : add error message code

--- core/accounts/forms.py
+++ core/accounts/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 from django import forms
+from django.core.exceptions import ValidationError
 
 class CustomUserCreationForm(UserCreationForm):
     username = forms.CharField(max_length=50, label="ID")
@@ -13,3 +14,13 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
         fields = ("username", "surname", "password1", "password2", "email", "message")
+        
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password_confirm = cleaned_data.get("password_confirm")
+
+        if password != password_confirm:
+            raise ValidationError("비밀번호가 일치하지 않습니다.")
+
+        return cleaned_data
