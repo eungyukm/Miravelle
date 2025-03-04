9650ee1f6cd03a6cfa036837616a47360f76d4d0
eungyu kim <86348187+eungyukm@users.noreply.github.com>
2025-03-04 17:22:34 +0900

    fix: error fixing
    ERRORS:
    assets.Asset.thumbnail: (fields.E210) Cannot use ImageField because Pillow is not installed.
            HINT: Get Pillow at https://pypi.org/project/Pillow/ or run command "python -m pip install Pillow".

--- core/model_storage/utils.py
+++ core/model_storage/utils.py
@@ -1,5 +1,7 @@
 from azure.storage.blob import BlobServiceClient
 import os
+from dotenv import load_dotenv
+
 
 # Storage Account Name과 Key 직접 사용
 AZURE_STORAGE_ACCOUNT_NAME = os.getenv("AZURE_STORAGE_ACCOUNT_NAME")
@@ -25,3 +27,22 @@ def upload_fbx_to_azure(file_path, blob_name):
         return f"업로드 성공: {blob_name}"
     except Exception as e:
         return f"업로드 실패: {str(e)}"
+    
+def download_fbx_from_azure(blob_name, local_file_path):
+    """
+    Azure Blob Storage에서 FBX 파일 다운로드
+
+    :param blob_name: Azure Blob에 저장된 파일명
+    :param local_file_path: 로컬에 저장할 파일 경로
+    """
+    try:
+        blob_service_client = BlobServiceClient.from_connection_string(AZURE_CONNECTION_STRING)
+        blob_client = blob_service_client.get_blob_client(container=AZURE_CONTAINER_NAME, blob=blob_name)
+
+        with open(local_file_path, "wb") as file:
+            file.write(blob_client.download_blob().readall())
+
+        return f"Download successful: {local_file_path}"
+
+    except Exception as e:
+        return f"Download failed: {str(e)}"
