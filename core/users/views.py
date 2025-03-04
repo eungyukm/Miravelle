f8b02d4f677b5e6fb217f0b2fe29bdb95d2d952e
배민경 <bmk0703@naver.com>
2025-03-04 19:00:13 +0900

    feat : add Register view code

--- core/users/views.py
+++ core/users/views.py
@@ -2,11 +2,28 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.decorators import login_required
-from .forms import CustomAuthenticationForm
+from .forms import CustomAuthenticationForm, CustomUserCreationForm
+from django.urls import reverse
 
 # 메인 화면
 def main(request):
     return render(request, "main.html")
+
+
+# 회원가입
+def Register(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)   # 바인딩 form
+        if form.is_valid():
+            user = form.save()
+            # 로그인 페이지 URL을 가져옴
+            login_url = reverse("users:login")
+            return redirect(login_url)
+    else:
+        form = CustomUserCreationForm()
+    context = {'form': form}
+    return render(request, "register.html", context)
+
 
 # 로그인
 def login(request):
