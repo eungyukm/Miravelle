deddf22f3bb66d594242720f78645666fff044f6
배민경 <bmk0703@naver.com>
2025-03-04 18:59:52 +0900

    feat : add register url

--- core/users/urls.py
+++ core/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import main, login, logout
+from .views import main, Register, login, logout
 
 app_name = "users"
 urlpatterns = [
     path("main/", main, name="main"), # 메인 화면
+    path("register/", Register, name="register" ), # 회원 가입
     path("login/", login, name="login"), # 로그인
     path("logout/", logout, name="logout"), # 로그아웃
 ]
