c884c6e9557ea46e850e28650bf3e70c1c4cb644
배민경 <bmk0703@naver.com>
2025-03-04 18:55:44 +0900

    delete : accounts app

--- core/accounts/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-class User(AbstractUser):
-    username = models.CharField(max_length=50, unique=True) # ID
-    surname = models.CharField(max_length=50) # Name
-    email = models.EmailField(unique=True) # 중복 불가
-    message = models.TextField()
--- core/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf import urls
-from django.urls import path
-from . import views
-
-app_name = "accounts"
-urlpatterns = [
-    path("register/", views.Register, name="register" ) # 회원 가입
-]
