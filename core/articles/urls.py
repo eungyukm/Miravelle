2d0b066a03b9343a0c577cfc2f66b8901d3b427f
배민경 <bmk0703@naver.com>
2025-03-05 12:19:39 +0900

    feat : add static url

--- core/articles/urls.py
+++ core/articles/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from .views import ArticleList
 
 app_name = "articles"
 urlpatterns = [
     path("articlelist/", ArticleList, name="articlelist"), # 게시물 목록
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
