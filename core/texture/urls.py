e2f4450d1885a265ae0de354aebc1bbdf836ba58
배민경 <bmk0703@naver.com>
2025-03-10 20:55:35 +0900

    fix : str texture_id
    int -> str

--- core/texture/urls.py
+++ core/texture/urls.py
@@ -3,5 +3,5 @@ from .views import upload_model, check_status
 
 urlpatterns = [
     path("upload/", upload_model, name="upload-model"),
-    path("status/<int:texture_id>/", check_status, name="check-status"),
+    path("status/<str:texture_id>/", check_status, name="check-status"),
 ]
