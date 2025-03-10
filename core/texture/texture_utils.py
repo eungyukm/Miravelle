e0f57095634f3a56cdd63146785cd8cb3e935e19
배민경 <bmk0703@naver.com>
2025-03-10 20:55:11 +0900

    fix : API_KEY url

--- core/texture/texture_utils.py
+++ core/texture/texture_utils.py
@@ -2,10 +2,10 @@ import requests  # HTTP 요청을 보내기 위한 라이브러리
 
 """Meshy API에 요청을 보내고, 작업 ID를 받아옴"""
 import requests
-from django.conf import settings
+from workspace.meshy_utils import call_meshy_api
 
 MESHY_API_URL = "https://api.meshy.ai/openapi/v1/text-to-texture"
-API_KEY = settings.MESHY_API_KEY  # 환경 변수에서 API 키 가져오기
+API_KEY = call_meshy_api  # utils 앱에서 AzureKeyManager가 API 데려옴
 
 def send_texture_request(model_url, object_prompt, style_prompt):
     payload = {
@@ -21,5 +21,6 @@ def send_texture_request(model_url, object_prompt, style_prompt):
     headers = {"Authorization": f"Bearer {API_KEY}"}
 
     response = requests.post(MESHY_API_URL, headers=headers, json=payload)
+    print("response.request.url : ", response.request.url)
     response.raise_for_status()  # 오류 발생 시 예외 처리
     return response.json()  # {'task_id': '018a210d-8ba4-705c-b111-1f1776f7f578'}
