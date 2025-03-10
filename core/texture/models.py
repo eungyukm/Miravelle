434800d2abc0ed859a480cb07ca35c65b6c55f3d
배민경 <bmk0703@naver.com>
2025-03-10 20:54:52 +0900

    fix : model_file url

--- core/texture/models.py
+++ core/texture/models.py
@@ -5,7 +5,7 @@ from django.conf import settings
 """요청을 DB에 저장하고 처리 상태를 추적하도록 설계"""
 class TextureRequest(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # 유저 정보
-    model_file = models.FileField(upload_to="/texture/models/")  # 3D 모델 업로드
+    model_file = models.FileField(upload_to="texture/models/")  # 3D 모델 업로드
     object_prompt = models.CharField(max_length=255)  # 오브젝트 설명
     style_prompt = models.TextField()  # 텍스처 스타일 설명
     task_id = models.CharField(max_length=100, blank=True, null=True)  # Meshy API 작업 ID
