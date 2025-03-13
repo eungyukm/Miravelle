8c170342dd1c92615ae659ebcefcfdc6bae4b34c
배민경 <bmk0703@naver.com>
2025-03-13 11:18:12 +0900

    feat : add decorators

--- core/texture/views.py
+++ core/texture/views.py
@@ -3,10 +3,12 @@ from django.http import JsonResponse
 from django.conf import settings
 from .models import TextureRequest
 from .texture_utils import send_texture_request, MESHY_API_URL
+from django.contrib.auth.decorators import login_required
 import requests
 
 
 """3D 모델 업로드 & API 요청 보내기"""
+@login_required
 def upload_model(request):
     if request.method == "POST":
         model_file = request.FILES["model_file"]
@@ -15,6 +17,7 @@ def upload_model(request):
 
         # DB에 저장
         texture_request = TextureRequest.objects.create(
+            user_id = request.user.id,
             model_file=model_file,
             object_prompt=object_prompt,
             style_prompt=style_prompt
@@ -23,6 +26,7 @@ def upload_model(request):
         # API 요청
         model_url = request.build_absolute_uri(texture_request.model_file.url)
         response = send_texture_request(model_url, object_prompt, style_prompt)
+        print("MESHY_API_URL:", response)
 
         # task_id 저장
         texture_request.task_id = response.get("task_id")
